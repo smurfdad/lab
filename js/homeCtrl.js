5ea88f8b96fb0337a2dddb4d2970abef66938ec5
Smurf Dad <smurfdad@smurfdad.es>
2015-08-21 10:43:39 +0200

    Refactorizamos para poder utilizar un map de objetos

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -5,44 +5,36 @@
 app.controller("homeCtrl", function ($scope,  $timeout, $interval, $log) {
 
   var REPOSITORIO = "https://lab-smurfdad.firebaseio.com/experimentos";
-  $scope.experimentos = new Array();
+  $scope.experimentos = {};
+  $scope.experimentosSize = function(){
+	var resultado = Object.keys($scope.experimentos).length;
+	$log.debug("Numero de experimentos: "+ resultado);
+	return resultado;
+  };
 
   var ref = new Firebase(REPOSITORIO);
   ref.on("child_added", function(snapshot) {
     $log.info("Recibiendo datos "+snapshot.key()+" "+snapshot.val().nombre);
     var valor = snapshot.val();
     var experimento = {};
-    experimento.nombre = snapshot.val().nombre;
-    experimento.descripcion = snapshot.val().descripcion;
-    experimento.href = snapshot.val().href;
-    experimento.id = snapshot.key();
+    // experimento.nombre = snapshot.val().nombre;
+    // experimento.descripcion = snapshot.val().descripcion;
+    // experimento.href = snapshot.val().href;
+    // experimento.id = snapshot.key();
     $scope.$apply(function(){
-        $scope.experimentos.push(experimento);
+        // $scope.experimentos.push(experimento);
+		$scope.experimentos[snapshot.key()] = snapshot.val();
     });
 
     //$scope.experimentos.push(experimento);
   });
 
-  ref.on("child_changed", function(snapshot) {
-    //setLoading();
-    var index = -1;
-    var encontrado = false;
-    for(var i= 0; i < $scope.experimentos.length && !encontrado; i++){
-      var experimento = $scope.experimentos[i];
-      $log.debug("Evaluando: "+snapshot.key() +" == " +experimento.id)
-      if (snapshot.key() == experimento.id){
-        $log.debug("Encontrado");
-        $scope.$apply(function(){
-          experimento.nombre = snapshot.val().nombre;
-          experimento.descripcion = snapshot.val().descripcion;
-          experimento.href = snapshot.val().href;
-        });
-        encontrado = true;
-      }
-    }
-    //$timeout(function(){
-    //  removeLoading();
-    //}, 500);
+	ref.on("child_changed", function(snapshot) {
+	$log.debug("Recibiendo datos");
+    $scope.$apply(function(){
+         $scope.experimentos[snapshot.key()] = snapshot.val();
+	});
+	$log.debug("Datos procesados");
   });
 
   ref.on("child_removed", function(snapshot) {
